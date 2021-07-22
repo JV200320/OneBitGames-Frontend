54d9370601053c9fea0bf44bb8b38ba0c62165ae
João Victor de Souza Lucena <joaovictordev2003@gmail.com>
2021-07-22 11:40:29 -0300

    Creating search reducer

--- components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
+++ components/shared/TitleAdminPanel/SearchAndIcon/index.tsx
@@ -5,30 +5,73 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import styles from '../../../../styles/AdminTitle.module.css';
 import { StyledButton } from '../../StyledButton';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import Link from 'next/link';
+
+import { useDispatch } from 'react-redux';
+import { setSearch as setSearchRedux, clearSearch as clearSearchRedux } from '../../../../store/modules/admin/shared/search/reducer';
+import { useRouter } from 'next/router';
+
 
 interface SearchAndIcon {
-  icon: IconProp
+  icon: IconProp,
+  newPath: string
 }
 
-export const SearchAndIcon: React.FC<SearchAndIcon> = ({ icon }) => {
+export const SearchAndIcon: React.FC<SearchAndIcon> = ({ newPath, icon }) => {
+
+  const [search, setSearch] = React.useState('');
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    dispatch(clearSearchRedux());
+  }, [])
+
+  const handleSearch = (): void => {
+    router.replace(router.pathname, '?page=1');
+    dispatch(setSearchRedux(search))
+  }
+
   return (
     <Row>
       <Col lg={9} xs>
         <Row>
           <Col lg={9} xs={10}>
             <InputGroup>
-              <FormControl placeholder="Pesquisar" className={styles.input} />
+              <FormControl placeholder="Pesquisar"
+                className={styles.input}
+                value={search}
+                onChange={(evt: React.ChangeEvent<HTMLInputElement>) => {
+                  setSearchRedux(evt.target.value)
+                }}
+                onKeyPress={
+                  (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                    if (evt.key.toLocaleLowerCase() === 'enter') {
+                      handleSearch();
+                    }
+                  }
+                }
+              />
             </InputGroup>
           </Col>
 
           <Col lg={3} xs={2} className="mt-1" style={{ cursor: 'pointer' }}>
-            <FontAwesomeIcon icon={faSearch} size="lg" color="var(--color-gray-light)" className="float-start" />
+            <FontAwesomeIcon icon={faSearch}
+              size="lg"
+              color="var(--color-gray-light)"
+              className="float-start"
+              onClick={handleSearch}
+            />
           </Col>
         </Row>
       </Col>
 
       <Col lg={2} xs={{ span: 3 }} className={styles.titleButton}>
-        <StyledButton icon={icon} type_button='blue' />
+        <Link href={newPath}>
+          <a>
+            <StyledButton icon={icon} type_button='blue' />
+          </a>
+        </Link>
       </Col>
     </Row>
   )
